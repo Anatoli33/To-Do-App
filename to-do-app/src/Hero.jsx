77bf2b192ad10b1cbd6933cb8ca862fd93b0f060
Anatoli33 <hadjiev1215@gmail.com>
2025-12-16 18:04:50 +0200

    Start the todo app

---
+++ to-do-app/src/Hero.jsx
@@ -0,0 +1,15 @@
+
+const Todo = () => {
+  return (
+    <section>
+        <h1>Tasks for today</h1>
+        <ul id="container">
+            <li>Drink water</li>
+            <li>Train</li>
+            <li>Wash the dishes</li>
+        </ul>
+    </section>
+  );
+};
+
+export default Todo;
