fb99dd575e412387771c21047717db73d87e9291
Anatoli33 <hadjiev1215@gmail.com>
2025-12-21 16:43:03 +0200

    About component

---
+++ to-do-app/src/About.jsx
@@ -0,0 +1,18 @@
+
+const About = () => {
+  return (
+    <section className="about">
+      <h2>About This Website</h2>
+      <p>
+        This website is a simple productivity app created to help users organize
+        their daily tasks and stay focused.
+      </p>
+      <p>
+        It is built with <strong>React</strong> and focuses on clean design,
+        simplicity, and fast interaction.
+      </p>
+    </section>
+  );
+};
+
+export default About;
